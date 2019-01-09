c2f0becb0cb526a7c47d4b06feeb67c87ccc8791
Lay <1778038380@qq.com>
2019-01-09 18:27:13 +0800

    spring boot docker部署测试

---
+++ spring-docker/src/main/java/com/lay/spring/docker/controller/DockerController.java
@@ -0,0 +1,19 @@
+package com.lay.spring.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: lay
+ * @Date: Created in 20:09 2019/1/8
+ * @Modified By:IntelliJ IDEA
+ */
+@RestController
+public class DockerController {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello Docker!";
+    }
+
+}
