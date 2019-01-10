6475b39d5d06f79e1b0870a53adcb34d162be8da
Lay <1778038380@qq.com>
2019-01-10 16:14:00 +0800

    async异步及异常处理

---
+++ spring-async/src/main/java/com/lay/spring/async/SpringAsyncApplication.java
@@ -0,0 +1,14 @@
+package com.lay.spring.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAsyncApplication.class, args);
+    }
+
+}
+
